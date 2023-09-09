8d655463c46cea5a76a1943d489c30c41c27b106
Ark _Ang3l <118340058+ark4ng3l@users.noreply.github.com>
2023-09-09 15:42:24 +0330

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,68 +1,75 @@
 import pyfiglet
-import os 
-import requests
 import colorama
 import subprocess
-import platform
-from cryptography.fernet import Fernet
-import sys
-import socket
-import winreg as wrg
-from tkinter import *
-from scapy.all import *
-import whois
-from tkinter import messagebox
+import os
 
-banner = pyfiglet.figlet_format('netcat')
+
+banner = pyfiglet.figlet_format('sup _ RAT')
 print (colorama.Fore.CYAN + banner)
-print (colorama.Fore.RED + 'Hi, I Can Do: ')
+#print (colorama.Fore.RED + 'Hi, I Can Do: ')
 
-menu_item_choise=['1','2','3','4','5','6']
+menu_item_choise_1=['1','2']
 
-menu_items='''
-[1] View system information
-[2] Get path to creat 100 folders
-[3] Get command and show output
-[4] Get program name and kill
-[5] Get site name and list virtual directory
-[6] exit
+menu_items1='''
+[1] Active Mode
+[2] Passive Mode
 '''
-print (colorama.Fore.YELLOW + menu_items)
+print (colorama.Fore.YELLOW + menu_items1)
 
 user_input = input(colorama.Fore.GREEN + "select a number: " + colorama.Fore.RESET)
-
-while user_input not in menu_item_choise:
-    print (colorama.Fore.CYAN + banner)
-    print (colorama.Fore.RED + 'Hi, I Can Do: ')
-    print (colorama.Fore.YELLOW + menu_items)
-    user_input = input(colorama.Fore.GREEN + "select a number: " + colorama.Fore.RESET)
 if user_input == '1':
-    print (os.uname_result)
-elif user_input == '2':
-    user_directory=input("Enter your directory to creat 100 folder: ")
-    os.chdir(user_directory)
-    for i in range(100):
-        os.mkdir(f"new folder {i}")
-elif user_input == '3':
-    command = input("Enter your command: ")
-    output_command = subprocess.check_output(command,shell=True).decode().split()
-    print(output_command)
-elif user_input == '4':
-    program_name = input('Enter name of pragram to kill it: ')
-    os.system(f" taskkill /f /im {program_name}*")
-elif user_input == '5':
-    site_name = input("enter site name: ")
-    if 'www' not in site_name:
-        site_name = 'www.' + site_name
-    if 'http://' not in site_name:
-        site_name = 'http://' + site_name
-    common = ['index.html' , 'robots.txt' , 'owa' , 'wp-admin']
-    found = []
-    for j in common:
-        r=requests.get(site_name + '/' + j)
-        if r.status_code == 200:
-            found.append(j)
-    print(found)
-elif user_input == '6':
-    print( 'Nice To Meet You :)')
-    exit()
+    pass
+elif user_input =='2':
+    menu_item_choise=['1','2','3','4','5','6']
+
+    menu_items='''
+    [1] View system information
+    [2] Get path to creat X folders
+    [3] Get command and show output
+    [4] Get program name and kill
+    [5] Get site name and list virtual directory
+    [6] exit
+    '''
+    os.system('cls')
+    print (colorama.Fore.CYAN + banner)
+    print (colorama.Fore.RED + 'PAssive OPTIONs: ')
+    print (colorama.Fore.YELLOW + menu_items)
+
+    user_input = input(colorama.Fore.GREEN + "select a number: " + colorama.Fore.RESET)
+
+    while user_input not in menu_item_choise:
+        print (colorama.Fore.CYAN + banner)
+        print (colorama.Fore.RED + 'Hi, I Can Do: ')
+        print (colorama.Fore.YELLOW + menu_items)
+        user_input = input(colorama.Fore.GREEN + "select a number: " + colorama.Fore.RESET)
+    if user_input == '1':
+        print (os.uname_result)
+    elif user_input == '2':
+        user_directory_number=int(input("Enter your directory to creat X folder: "))
+        user_directory=input("Enter your directory to creat X folder: ")
+        os.chdir(user_directory)
+        for i in range(user_directory_number):
+            os.mkdir(f"new folder {i}")
+    elif user_input == '3':
+        command = input("Enter your command: ")
+        output_command = subprocess.check_output(command,shell=True).decode().split()
+        print(output_command)
+    elif user_input == '4':
+        program_name = input('Enter name of pragram to kill it: ')
+        os.system(f" taskkill /f /im {program_name}*")
+    elif user_input == '5':
+        site_name = input("enter site name: ")
+        if 'www' not in site_name:
+            site_name = 'www.' + site_name
+        if 'http://' not in site_name:
+            site_name = 'http://' + site_name
+        common = ['index.html' , 'robots.txt' , 'owa' , 'wp-admin']
+        found = []
+        for j in common:
+            r=requests.get(site_name + '/' + j)
+            if r.status_code == 200:
+                found.append(j)
+        print(found)
+    elif user_input == '6':
+        print( 'Nice To Meet You :)')
+        exit()
